4680619b6599c100654d4cbd1be44b5b2b6a2e41
mattos-rafael <rafa.rafa.rafa07@gmail.com>
2026-05-11 13:47:27 +0200

    added cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const cookieParser = require("cookie-parser")
+const cors = require('cors')
 
 const characterRoutes = require('./routes/character.routes')
 const authRoutes = require('./routes/auth.routes')
 const userRoutes = require('./routes/user.routes')
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use('/api/character', characterRoutes)
 app.use('/api/auth', authRoutes)
