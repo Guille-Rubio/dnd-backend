c49c27069c316e03d61987bfe696e50477b84cd0
mattos-rafael <rafa.rafa.rafa07@gmail.com>
2026-05-13 13:46:52 +0200

    add auth headers to morgan

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const userRoutes = require('./routes/user.routes')
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 const app = express()
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+morgan.token('auth', (req) => req.headers['authorization'] || 'No Auth Header');
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :auth'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
